02b6c84741f22f33f33143ceb4330e721cd218dd
aqsaprmn <aqshapermana@gmail.com>
2022-06-22 16:00:59 +0700

    First Commit

---
+++ cluster4.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'connect.php';
+
+mysqli_query($conn, "INSERT INTO qr VALUES ('', 'Cluster4',now())");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Cluster 4
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'connect.php';
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        * {
+            /* margin: 0;
+            padding: 0; */
+        }
+
+        html,
+        body {
+            height: 100%;
+            /* font-family: 'Poppins', sans-serif; */
+        }
+
+        div {
+            /* border: 1px solid black; */
+            box-sizing: border-box;
+            /* text-align: center; */
+            height: 100%;
+            /* display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center; */
+            color: black;
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <h1>File Not Found - 404</h1>
+        <p>Forbidden to access this page</p>
+    </div>
+</body>
+
+</html>
